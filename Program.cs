5a371a7383eb9bb4e632ff86bd2e4c7a495282e0
Pao-Esquinca <pl836507@gmail.com>
2023-10-05 19:47:25 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L.S.C_3K_Ejer04_Cgato
+{
+    using System;
+
+    class program
+    {
+        const int maxf = 3;
+        const int maxc = 5;
+
+        static void Main(string[] args)
+        {
+            float[,] a = new float[maxf, maxc];
+            int f, c;
+            //leer el array
+            for (f = 0; f < maxf; f++)
+            {
+                for (c = 0; c < maxc; c++)
+                {
+                    a[f, c] = float.Parse(Console.ReadLine());
+                }
+            }
+            //Escribir el array
+            for (f = 0; f < maxf; f++)
+            {
+                for (c = 0; c < maxc; c++)
+                {
+                    Console.Write(a[f, c] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
